7e7036c7b01b83fd36abc011fc406f55328992fc
Eddev <eduardemian777@gmail.com>
2023-10-03 21:21:06 -0300

    feat: :sparkles: add Inicio page

--- bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/pages/Inicio.jsx
+++ bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/pages/Inicio.jsx
@@ -1,9 +1,27 @@
-import './Inicio.scss'
+import { useContext } from 'react';
+import ItemContext from '../contexts/ItemContext';
+import Card from '../components/Card';
+import './Inicio.scss';
 
 const Inicio = () => {
-  return (
-    <div>Inicio</div>
-  )
-}
+    const { items } = useContext(ItemContext);
 
-export default Inicio
+    return (
+        <main>
+            <section className='section-cards'>
+                <header className='section-cards__header'>
+                    <h1>Compra el planeta de tus sueños</h1>
+                    <p>¿Que esperas para tu terreno espacial?</p>
+                </header>
+
+                <div className='cards-container'>
+                    {items && items.map((item, id) => 
+                      <Card key = {id} propItems = {item} />
+                    )}
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default Inicio;
