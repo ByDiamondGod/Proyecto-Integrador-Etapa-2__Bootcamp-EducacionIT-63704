d71b9fd5ada0c17d32768aaaa3badefca1160035
Eddev <eduardemian777@gmail.com>
2023-10-03 21:19:39 -0300

    feat: :sparkles: add card

--- bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/components/Card.jsx
+++ bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/components/Card.jsx
@@ -1,9 +1,36 @@
+import PropTypes from 'prop-types';
 import './Card.scss'
 
-const Card = () => {
+const Card = ({ propItems }) => {
+  
   return (
-    <div>Card</div>
+    <>
+      <a href="#" className="card">
+          <article className="card__article">
+
+            <div className="card__image-container">
+              <img className="card__image" src={propItems.pic} alt={propItems.name} />
+            </div>
+
+            <div className="card__content">
+              <h2 className="card__heading">{propItems.name}</h2>
+              <div className="card__description">
+                <p>{propItems.description}</p>
+              </div>
+            </div>
+        </article>
+        
+        </a>
+    </>
   )
 }
 
-export default Card
+Card.propTypes = {
+  propItems: PropTypes.shape({
+    pic: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Card;
