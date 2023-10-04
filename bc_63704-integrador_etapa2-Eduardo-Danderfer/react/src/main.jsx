f96315abf93cd02198e0271d0f3fae878a42d4d9
Eddev <eduardemian777@gmail.com>
2023-10-03 21:26:07 -0300

    feat: :sparkles: add main with ItemProvider/BrowserRouter
    using components header and footer + imports pages and Provider

--- bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/main.jsx
+++ bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import './index.scss'
+
+import Inicio from './pages/Inicio'
+import Nosotros from './pages/Nosotros'
+import Alta from './pages/Alta'
+import Contacto from './pages/Contacto'
+import Carrito from './pages/Carrito'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { ItemProvider } from './contexts/ItemContext'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ItemProvider>
+    <BrowserRouter>
+      
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Inicio/>} />
+        <Route path='/alta' element={<Alta/>} />
+        <Route path='/contacto' element={<Contacto/>} />
+        <Route path='/nosotros' element={<Nosotros/>} />
+        <Route path='/carrito' element={<Carrito/>} />
+        <Route path='*' element={<Inicio/>} />  {/* Componente si hay un error se usa el * */}
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+    </ItemProvider>
   </React.StrictMode>,
 )
