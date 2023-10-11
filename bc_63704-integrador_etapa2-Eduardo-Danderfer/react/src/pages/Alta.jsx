9fa0a07b4c69a69c36fda9a9cc54ab0f37807bbc
Eddev <eduardemian777@gmail.com>
2023-10-11 13:08:51 -0300

    feat: :sparkles: Add page Alta

--- bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/pages/Alta.jsx
+++ bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/pages/Alta.jsx
@@ -1,9 +1,25 @@
-import './Alta.scss'
+import TypeAlta from '../components/typewritters/TypeAlta';
+
+import Formulario from '../components/formAlta/Formulario';
+import Tabla from '../components/formAlta/Tabla';
+
+import './Alta.scss';
 
 const Alta = () => {
-  return (
-    <div>Alta</div>
-  )
-}
+    return (
+        <main>    
+            <div className='heading'>
+                <header className='heading__header'>
+                    <h1>Alta</h1>
+                    <span> <TypeAlta/> </span>
+                </header>
+            </div>
+            <div>
+                <Formulario/>
+                <Tabla />
+            </div>
+        </main>
+    );
+};
 
-export default Alta
+export default Alta;
