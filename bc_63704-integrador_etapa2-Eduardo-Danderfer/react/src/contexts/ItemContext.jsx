0d1ebf265dc211f22c435e7b6bf300d15b89f38b
Eddev <eduardemian777@gmail.com>
2023-10-12 19:05:15 -0300

    feat: :zap: add ItemContext CRUD

--- bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/contexts/ItemContext.jsx
+++ bc_63704-integrador_etapa2-Eduardo-Danderfer/react/src/contexts/ItemContext.jsx
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { get } from '../utils/http';
+import { get, post, del, put } from '../utils/http';
 
 const ItemContext = createContext();
 
@@ -13,16 +13,56 @@ const ItemProvider = ({ children }) => {
         getItems();
     }, []);
 
+    // CRUD = READ ALL (GET)
+
     const getItems = async () => {
         try {
             const items = await get(url);
             setItems(items);
         } catch (error) {
-            console.log('Error found in getItems:', error);
+            console.error('Error found in getItems:', error);
         }
     };
 
-    const data = { items };
+    // CRUD = CREATE (POST)
+
+    // se usara dentro del formulario
+    const createItemContext = async newItem => {
+        try {
+            const createItem = await post(url, newItem);
+            setItems([...items, createItem]);
+        } catch (error) {
+            console.error('Error found in createItemContext:', error);
+        }
+    };
+
+    // CRUD = DELETE (DELETE)
+
+    const deleteItemContext = async id => {
+        try {
+            const deleteItem = await del(url, id);
+            console.log(deleteItem);
+            const nuevaDB = items.filter(item => item.id !== id);
+            setItems(nuevaDB);
+        } catch (error) {
+            console.error('Error found in deleteItemContext:', error);
+        }
+    };
+
+    // CRUD = UPDATE (PUT) [80]
+
+    const updateItemContext = async itemToUpdate => {
+        try {
+            const updateItem = await put(url, itemToUpdate.id, itemToUpdate);
+            console.log(updateItem);
+            const nuevaDB = items.map(item => item.id === itemToUpdate.id ? itemToUpdate : item);
+            setItems(nuevaDB);
+        } catch (error) {
+            console.error('Error found in updateItemContext:', error);
+        }
+    }
+
+    const data = { items, createItemContext, deleteItemContext, updateItemContext }; // consumir informacion del CRUD
 
     return <ItemContext.Provider value={data}>{children}</ItemContext.Provider>;
 };
